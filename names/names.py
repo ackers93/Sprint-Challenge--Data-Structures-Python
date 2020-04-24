94c8c4b11b5c5f3ad7e74307da4d35cdfffac492
Andrew Ackerman <andrew.ackers@gmail.com>
2020-04-24 09:59:50 -0700

    names mvp met

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from lru_cache import LRUCache
 
 start_time = time.time()
 
@@ -13,14 +14,22 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+lru = LRUCache(10000)
+for index, name in enumerate(names_1):
+    lru.set(name, index)
+for name_2 in names_2:
+    lru.get(name_2, duplicates)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+# runtime: 0.03225588798522949 seconds
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
